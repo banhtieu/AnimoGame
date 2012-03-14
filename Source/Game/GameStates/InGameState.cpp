2e353f81c80dff6bc18aca2708425913fe3190e0
banhtieu <just@banhtieu.com>
2012-03-14 21:36:06 +0700

    + Make all data from script.

---
+++ Source/Game/GameStates/InGameState.cpp
@@ -0,0 +1,9 @@
+//
+//  InGameState.cpp
+//  AnimoGame
+//
+//  Created by Tran Khiem on 3/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ Source/Game/GameStates/InGameState.h
@@ -0,0 +1,14 @@
+//
+//  InGameState.h
+//  AnimoGame
+//
+//  Created by Tran Khiem on 3/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef AnimoGame_InGameState_h
+#define AnimoGame_InGameState_h
+
+
+
+#endif
